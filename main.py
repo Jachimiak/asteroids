29c1597a13aee5dc54caf6696d9217a0289e5d8b
Terry Jachimiak II <terry.jachimiak@gmail.com>
2025-07-15 13:38:04 -0400

    ADD Asteroid creation

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -14,17 +16,35 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+    
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
+    
 
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
         
+        
+        updatable.update(dt)
+
         pygame.Surface.fill(screen, (0, 0, 0))
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+        
 
         pygame.display.flip()
 
